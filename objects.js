a655b513344a00b4cf3092bb388ff60141dfb02a
Cassandra Corrales <cmcorrales3@gmail.com>
2018-01-16 03:37:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,5 +11,4 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
   updatePlaylist();
-  return playlist;
 }
